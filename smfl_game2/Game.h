44639e09387a270ccd8accc23471ea5d9d40d5e6
mjpitts <mjpitts@ucdavis.edu>
2023-10-09 13:13:35 -0700

    Add project files.

---
+++ smfl_game2/Game.h
@@ -0,0 +1,24 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <iostream>
+#include <ctime>
+
+// SFML includes
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Network.hpp>
+
+class Game {
+public:
+	Game();
+	~Game();
+
+private:
+
+};
+
+#endif
+
---
+++ smfl_game2/smfl_game2.cpp
@@ -0,0 +1,13 @@
+#include "Game.h"
+
+int main()
+{
+    // Init srand seed
+    srand(static_cast<unsigned>(time(NULL)));
+
+    // Init game object
+    Game game;
+
+
+    return 0;
+}
